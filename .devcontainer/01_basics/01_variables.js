cf265210873ba23edc66e70762267caf9fcc0688
Harshitpatel111 <155393115+Harshitpatel111@users.noreply.github.com>
2025-06-16 12:32:00 +0000

    discussion on variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "harshit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rajkot"
+let accountState;
+
+//accountId = 2
+
+
+accountEmail = "hb@gmail.com"
+accountPassword  = "23456"
+accountCity = "Pune"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
