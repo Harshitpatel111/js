d480f0712ffaca71189ea041fea06f59245b5092
Harshitpatel111 <155393115+Harshitpatel111@users.noreply.github.com>
2025-06-16 13:49:03 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "3abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score) //for conversion First letter is capital
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+ //here when the 3abc is passed it will not get converted so will display NaN but if null passed then will show 0
+ //"33"=>33
+ //"3abc"=>NaN
+ //true=>1;false=>0
+
+let isLoggedIn = "h"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "h" => true
