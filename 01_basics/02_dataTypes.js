0c971253f75a30b89536ff9512661f4862a90deb
Harshitpatel111 <155393115+Harshitpatel111@users.noreply.github.com>
2025-06-16 12:58:20 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3 + 3)  // we are using node js ,not browser
+
+console.log(3
+    +
+    3) // readability sholud be high 
+
+console.log("Harshit")
+
+
+let name ="harshit" //string
+let age= 18 //number
+let isLoggedIn=false//boolean
+let state;//undefined
+let sabc=null;//here empty that is might be zero not undefined 
+//number=>2 to power 53
+//bigint 
+//string =>""
+//boolean=> true/false
+//null =>standalone value representation of empty value
+//undefined =>
+//symbol =>unique 
+
+
+//object 
+
+console.log(typeof "bbb") // to determine the type of datatype
+
+//interview question can be asked that type of undefined is undefined but for null the type is OBJECT
