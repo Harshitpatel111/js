54416c3dcbefd9049d74bfa2529565a1b05eeb7d
Harshitpatel111 <155393115+Harshitpatel111@users.noreply.github.com>
2025-06-17 06:36:02 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+ console.log("2" > 1);
+ console.log("02" > 1);
+
+ // to avoid the following comparision in code from line 11 to 17
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === it is used for strict check that not only values but also datatypes
+
+//console.log("2" === 2);
