a5cd790ed466c3048eaffde9136388fd6e34c89f
Harshitpatel111 <155393115+Harshitpatel111@users.noreply.github.com>
2025-06-17 03:54:18 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "3abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score) //for conversion First letter is capital
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
  //here when the 3abc is passed it will not get converted so will display NaN but if null passed then will show 0
  //"33"=>33
@@ -20,3 +20,23 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // 1 => true; 0 => false
 // "" => false
 // "h" => true
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+let str1="hello"
+let str2=" world"
+let str3=str1+str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+//note: when string is first it converts all after them string but if it is afterwards then it is done after the string occurs
+
+//console.log(+true);  + is used for conversion
+
+
+
+
